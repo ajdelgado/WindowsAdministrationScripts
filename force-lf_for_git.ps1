19cdbf9f7222a4b7b049cb9bfda34eca24107ac8
Antonio J. Delgado <antoniodelgado@susurrando.com>
2020-11-23 10:31:46 +0200

    Force LF

---
+++ force-lf_for_git.ps1
@@ -0,0 +1,6 @@
+# Force Git to use LF everywhere
+#https://www.cylindric.net/git/force-lf
+git config core.eol lf
+git config core.autocrlf input
+git config --global core.eol lf
+git config --global core.autocrlf input
