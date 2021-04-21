42ac51d0afc9d063ae84c65965d180f27a33a990
Antonio J. Delgado <antoniodelgado@susurrando.com>
2021-04-21 15:12:18 +0300

    Add require admin and remove temp chocolatey folder

--- Upgrade-ChocolateyPackages.ps1
+++ Upgrade-ChocolateyPackages.ps1
@@ -1,3 +1,5 @@
+#Requires -RunAsAdministrator
+Remove-Item -Recurse -Path $env:windir\temp\chocolatey
 foreach ($pkgline in (choco list --local-only | select-string "^[a-zA-Z0-9\-]* [0-9\.]*$"))
 {
     $pkg = ($pkgline -split " ")[0]
