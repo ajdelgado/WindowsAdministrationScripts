12257f1275f1b3fa9e3214994921a76cf62f27dd
Antonio J. Delgado <ad@susurrando.com>
2024-01-08 18:45:05 +0200

    Remove old filter for local

--- Upgrade-ChocolateyPackages.ps1
+++ Upgrade-ChocolateyPackages.ps1
@@ -1,6 +1,6 @@
 #Requires -RunAsAdministrator
 Remove-Item -Recurse -Path "$env:windir\temp\chocolatey"
-foreach ($pkgline in (choco list --local-only | select-string "^[a-zA-Z0-9\-]* [0-9\.]*$"))
+foreach ($pkgline in (choco list | select-string "^[a-zA-Z0-9\-]* [0-9\.]*$"))
 {
     $pkg = ($pkgline -split " ")[0]
     # Adobe Reader doesn't update, so avoiding it and also all patches that should NOT need updates
